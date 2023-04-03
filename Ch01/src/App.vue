c6a8124d318907ddf0cc323d1a9eb16cf440f807
aowlrxm15 <aowlrxm15@gmail.com>
2023-04-03 18:14:22 +0900

    23/03-03

---
+++ Ch01/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h3>Ch01.Vue 소개</h3>
+  <p>Hello World</p>
+</template>
+
+<script>
+/**
+ * 날짜 : 2023/04/03
+ * 이름 : 전인준
+ * 내용 : Ch01.Vue.js 소개
+ *
+ * 개발환경 설정
+ * - node.js 설치
+ * - VSCode 설치 및 확장 플러그인(volar, vue3 snippets, prettier, ESLint)
+ * - Vue.js devtools Chrome 확장 플러그인 설치
+ *
+ * 프로젝트 생성/실행
+ * - npm init vite-app "Ch01"
+ * - cd Ch01
+ * - npm install
+ * - npm run dev
+ * 배포
+ * - npm run duild - dist폴더 생성
+ *
+ * prettier 안될때 fomat on save 체크 and default fomatter prettier로 설정
+ */
+export default {
+  name: "App",
+};
+</script>
