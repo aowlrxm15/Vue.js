5d3eac3a8b4549420b7344e11f832426c4a2ec7c
aowlrxm15 <aowlrxm15@gmail.com>
2023-04-07 17:03:39 +0900

    2023-04-07

---
+++ Ch05/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h3>Ch05.Router 실습</h3>
+
+  <h4>1) Router 기본</h4>
+  <router-link to="/hello">Hello</router-link>&nbsp;/
+  <router-link to="/Welcome">Welcome</router-link>&nbsp;/
+  <router-link to="/Greeting">Greeting</router-link>
+
+  <router-view></router-view>
+  <hr />
+
+  <h4>2) Nested Router</h4>
+  <hr />
+
+  <h4>3) Named View</h4>
+  <hr />
+</template>
+<script setup></script>
+
+<style scoped></style>
