b61d1460641977e21e10b40ee3724424c412535c
aowlrxm15 <aowlrxm15@gmail.com>
2023-04-10 16:48:27 +0900

    2023-04-10

--- Ch05/src/App.vue
+++ Ch05/src/App.vue
@@ -6,14 +6,40 @@
   <router-link to="/Welcome">Welcome</router-link>&nbsp;/
   <router-link to="/Greeting">Greeting</router-link>
 
-  <router-view></router-view>
   <hr />
 
   <h4>2) Nested Router</h4>
+  <router-link to="/parent">Parent</router-link>
   <hr />
 
   <h4>3) Named View</h4>
+  <router-link to="/main1">Main1</router-link>&nbsp;/
+  <router-link to="/main2">Main2</router-link>&nbsp;/
+  <router-link to="/main3">Main3</router-link>&nbsp;/
   <hr />
+
+  <h4>4) Router 데이터 전송</h4>
+  <router-link
+    :to="{
+      name: 'Query',
+      query: { uid: 'a101', name: '홍길동', age: 21 },
+    }"
+    >Query</router-link
+  >&nbsp;/
+
+  <router-link
+    :to="{
+      name: 'Param',
+      params: { uid: 'a101', name: '홍길동', age: 21 },
+    }"
+    >Param</router-link
+  >&nbsp;/ <router-link to="/form1">Form1</router-link>&nbsp;/
+  <router-link to="/form2">Form2</router-link>
+
+  <h4>라우팅 컴포넌트 출력</h4>
+  <router-view name="header"></router-view>
+  <router-view></router-view>
+  <router-view name="footer"></router-view>
 </template>
 <script setup></script>
 
